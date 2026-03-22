ddf54f252963150ed6823fd33600e3451f48678f
dakies <dkiesewalter97@gmail.com>
2026-03-22 23:19:06 +0100

    Flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Astro website dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_22
+            nodePackages.npm
+          ];
+        };
+      });
+}
