e1a304bbfe8f4363e3d744748c2ea8497813d1cc
dakies <dkiesewalter97@gmail.com>
2026-03-23 13:54:13 +0100

    Actions runner

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: "https://verion-eda.com",
   integrations: [tailwind()],
 });
